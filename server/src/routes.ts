d1bacd2582ca1a80cd11eda7ee146926edc41075
Silva <Abimael_Silva1@Dell.com>
2020-06-04 21:11:19 -0300

    First commit, day 1 and day 2/1

---
+++ server/src/routes.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import connection from "./database/connection";
+
+const routes = express.Router();
+
+routes.get('/items', async (request, response) => {
+    const items = await connection('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    });
+
+    return response.json(serializedItems);
+});
+
+export default routes;
+
